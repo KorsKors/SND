a195a75bbfa6a12e72b0d74d958a0e56b19744c3
Huskar <korsdecaying@gmail.com>
2017-03-23 16:55:45 +0300

    Чистая версия запрос к Elasticsearch.выгрузка по 3 параметрам.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-/**
- * Created by Хускар on 21.03.2017.
- */
 import org.apache.lucene.queryparser.flexible.core.builders.QueryBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.*;
@@ -8,6 +5,7 @@ import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.filters.FiltersAggregator;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import  org.elasticsearch.transport.* ;
 import org.elasticsearch.client.transport.*;
 import org.elasticsearch.common.settings.Settings;
@@ -23,49 +21,66 @@ import java.util.Map;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
 public class Main {
-
-
-    static TransportClient  client;
     public static void main(String[] args){
-        Map<String, Object> res=new HashMap<String, Object>();;
-                try {
-                      client = new PreBuiltTransportClient(Settings.EMPTY)
-            //Client client=TransportClient.
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
-                   // .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host2"), 9300));
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        //System.out.println("22");
-// on shutdown
-        //SearchResponse response = client.prepareSearch("packetbeat-2017.03.12").setTypes("DNS").setQuery().get();
-        //SearchResponse response = client.prepareSearch(String.valueOf(QueryBuilders.matchAllQuery()))
-          //      .setQuery();
-        SearchResponse response = client.prepareSearch("packetbeat-2017.03.12")
-                .setQuery(QueryBuilders.rangeQuery("@timestamp").gte("now-12d").lt("now-10d"))
-               //.addAggregation(AggregationBuilders.terms("dns.question.type").subAggregation(AggregationBuilders.count("dns.question.type"))).get();
-                //.addAggregation(QueryBuilders.termsQuery("dns.question.type","",)).get();
-               // .addAggregation(AggregationBuilders.terms("dns.question.type").subAggregation(AggregationBuilders.cardinality("agg").field("dns.question.type"))).get();
-                .addAggregation(AggregationBuilders
-                        .terms("q").field("dns.question.type")).execute().actionGet();
-                Terms dns_question_type=response.getAggregations().get("q");
-                for (Terms.Bucket entry : dns_question_type.getBuckets()) {
-                                 res.put(entry.getKeyAsString(),entry.getDocCount());
-                    System.out.println(entry.getKeyAsString()+entry.getDocCount());
-                }
-                                 //entry.getKey();      // Term
-                                 //entry.getDocCount(); // Doc count
-                //.setQuery(QueryBuilders.rangeQuery("@timestamp").gte("1489303962532").lt("1489344462532").format("epoch_millis")).get();
-                //.addAggregation(AggregationBuilders.terms("dns.question.type").field("dns.question.type").subAggregation()).get();
+        TransportClient  client;
+        Map<String, Object> res=new HashMap<String, Object>();
+            try {
+                client = new PreBuiltTransportClient(Settings.EMPTY)
+                        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
 
-                //.addAggregation(QueryBuilders.termsQuery("dns.question.type",AggregationBuilders.count("desk"))).get();
-        System.out.println("33");
-      //  SearchResponse response = client.prepareSearch(String.valueOf(QueryBuilders.matchAllQuery()))
-         //       .setQuery()
-           //     .get();
-        SearchHit[] hits = response.getHits().getHits();
-       Map<String, Object> result = hits[0].sourceAsMap();
-        System.out.println("44");
-        System.out.print(result.get(0));
-    }}//.setQuery(String.valueOf(QueryBuilders.rangeQuery("@timestamp").gte("1489303962532").lt("1489344462532").format("epoch_millis")))
-//client.close();
+        SearchResponse response = client.prepareSearch("packetbeat-2017.03.12")
+            .setQuery(QueryBuilders.rangeQuery("@timestamp").gte("now-12d").lt("now-10d"))
+                .addAggregation(AggregationBuilders
+                    .terms("name").field("dns.question.name").size(5))
+                .addAggregation(AggregationBuilders
+                    .terms("type").field("dns.question.type").size(5))
+                    .addAggregation(AggregationBuilders
+                    .terms("fullname").field("dns.question.etld_plus_one").size(5))
+            .setPostFilter(QueryBuilders.rangeQuery("@timestamp").gte("now-12d").lt("now-10d"))
+                .addAggregation(AggregationBuilders
+                    .terms("namepost").field("dns.question.name").size(2))
+                .addAggregation(AggregationBuilders
+                    .terms("typepost").field("dns.question.type").size(2))
+                .addAggregation(AggregationBuilders
+                    .terms("fullnamepost").field("dns.question.etld_plus_one").size(2))
+            .execute().actionGet();
+
+        Terms dns_question_fullname=response.getAggregations().get("fullname");
+        Terms dns_question_name=response.getAggregations().get("name");
+        Terms dns_question_types=response.getAggregations().get("type");
+
+        for (Terms.Bucket entry1 : dns_question_name.getBuckets()) {
+            res.put(entry1.getKeyAsString(),entry1.getDocCount());
+            System.out.println(entry1.getKeyAsString()+":"+entry1.getDocCount());
+        }
+        for (Terms.Bucket entry : dns_question_types.getBuckets()) {
+            res.put(entry.getKeyAsString(),entry.getDocCount());
+            System.out.println(entry.getKeyAsString()+":"+entry.getDocCount());
+                }
+        for (Terms.Bucket entry2 : dns_question_fullname.getBuckets()) {
+            res.put(entry2.getKeyAsString(),entry2.getDocCount());
+            System.out.println(entry2.getKeyAsString()+":"+entry2.getDocCount());
+        }
+
+        Terms dns_question_fullname_post=response.getAggregations().get("fullnamepost");
+        Terms dns_question_name_post=response.getAggregations().get("namepost");
+        Terms dns_question_types_post=response.getAggregations().get("typepost");
+
+        for (Terms.Bucket entry1 : dns_question_name_post.getBuckets()) {
+            res.put(entry1.getKeyAsString(),entry1.getDocCount());
+            System.out.println(entry1.getKeyAsString()+":"+entry1.getDocCount());
+        }
+        for (Terms.Bucket entry : dns_question_types_post.getBuckets()) {
+            res.put(entry.getKeyAsString(),entry.getDocCount());
+            System.out.println(entry.getKeyAsString()+":"+entry.getDocCount());
+        }
+        for (Terms.Bucket entry2 : dns_question_fullname_post.getBuckets()) {
+            res.put(entry2.getKeyAsString(),entry2.getDocCount());
+            System.out.println(entry2.getKeyAsString()+":"+entry2.getDocCount());
+        }
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+    }
+}
